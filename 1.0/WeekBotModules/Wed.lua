e5fd6f58faba339f639d2b7f0ff71b9ce3a5c981
John Sandler <priciel@protonmail.com>
2016-11-24 16:40:41 +0100

    Update Wed.lua

--- 1.0/WeekBotModules/Wed.lua
+++ 1.0/WeekBotModules/Wed.lua
@@ -1,9 +1,9 @@
 local date = {}
 
 function date:Init()
-	_G.avatar = "https://yt3.ggpht.com/-NLkFN_OWDCg/AAAAAAAAAAI/AAAAAAAAAAA/2Godw1Km5Qw/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"
-	_G.status = "Wednesday!!!"
-	_G.facts = {
+	date.avatar = "https://yt3.ggpht.com/-NLkFN_OWDCg/AAAAAAAAAAI/AAAAAAAAAAA/2Godw1Km5Qw/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"
+	date.status = "Wednesday!!!"
+	date.facts = {
 		[1] = "“Wednesday” is named after Woden, the most important God in the German pantheon, often identified with the Norse God Odin.",
 		[2] = "According to research by a tanning company, women look oldest at 3.30pm on Wednesdays. Yes, this research was done by a fairness cream company.",
 		[3] = "A recent survey in the US reported that bosses are most receptive to requests on a Wednesday.",
@@ -13,7 +13,7 @@ function date:Init()
 		[7] = "The Wednesday before Easter is known as ‘Spy Wednesday’ referring to Judas’s betrayal of Jesus.", 
 		[8] = "Wednesday is also referred as ‘hump day’ because this day comes in middle of the week and after that its downhill towards weekend.",
 	}
-	_G.color = 13979647
+	date.color = 13979647
 end
 
 return date
