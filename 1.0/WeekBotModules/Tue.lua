a0680443fed73e84aa7c4ce7d2b90dde6d3b0f21
John Sandler <priciel@protonmail.com>
2016-11-24 16:40:51 +0100

    Update Tue.lua

--- 1.0/WeekBotModules/Tue.lua
+++ 1.0/WeekBotModules/Tue.lua
@@ -1,16 +1,16 @@
 local date = {}
 
 function date:Init()
-	_G.avatar = "http://whitelion.bar/wp-content/uploads/2015/11/Tuesday-designstyle-colors-m-300x184.png"
-	_G.status = "Tuesday... getting closer to friday."
-	_G.facts = {
+	date.avatar = "http://whitelion.bar/wp-content/uploads/2015/11/Tuesday-designstyle-colors-m-300x184.png"
+	date.status = "Tuesday... getting closer to friday."
+	date.facts = {
 		[1] = "The day of the week Tuesday is named after Tiw, the Norse god of single combat, victory and glory.",
 		[2] = "2. Tiw is associated with Mars, the Roman god of war, which is why the day is Mardi in French, Martes in Spanish and Martedi in Italian.",
 		[3] = "Californians have barbecues less often on Tuesday than any other day of the week.",
 		[4] = "Under the rules of the Gregorian calendar, Christmas Eve falls less often on a Tuesday than any other day of the week...",
 		[5] = "According to a survey in 2002, Tuesday is the most productive day of the week in the workplace.",
 	}
-	_G.color = 13979647
+	date.color = 13979647
 end
 
 return date
