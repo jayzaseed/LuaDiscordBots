b18bec9bf04a6770ec390127b6d71f2cdb0aa52d
John Sandler <priciel@protonmail.com>
2016-11-25 18:29:26 +0100

    Update help.lua

--- 1.0/help.lua
+++ 1.0/help.lua
@@ -1,4 +1,35 @@
+WhichDay = os.date("%a")
+date = require("WeekBotModules/Week")
+
 return {
-	["!fact"] = "Typing !fact will make me send you a random fact from the current day!",
-	["!help"] = "I will send you the information about my commands.",
+	["!fact"] = {
+		["color"] = date[WhichDay].color,
+		["fields"] = {
+			{
+				name = "!fact", 
+				value =  "Typing !fact will make me send you a random fact from the current day!",
+				inline = false
+			},
+		},
+	},
+	["!help"] = {
+		["color"] = date[WhichDay].color,
+		["fields"] = {
+			{
+				name = "!help", 
+				value =  "I will send you the information about my commands.",
+				inline = false
+			},
+		},
+	},
+	["empty"] = {
+		["color"] = date[WhichDay].color,
+		["fields"] = {
+			{
+				name = "Available help commands", 
+				value =  "!help !fact\n!help !help.",
+				inline = false
+			},
+		},
+	},
 }
