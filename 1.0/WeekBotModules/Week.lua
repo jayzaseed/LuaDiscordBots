e4f360003f0009da6fc08a7671957ad830f9bddd
John Sandler <priciel@protonmail.com>
2016-11-25 18:15:50 +0100

    Add files via upload

---
+++ 1.0/WeekBotModules/Week.lua
@@ -0,0 +1,74 @@
+return {
+	["Mon"] = {
+		avatar = "http://az616578.vo.msecnd.net/files/2015/07/26/635735497317415146-502010203_image.imgopt1000x70.jpg",
+		status = "Oh crap it's monday...",
+		facts = {
+			[1] = "According to a study by Marmite, most people don't even smile any earlier than 11:16 AM on Monday mornings. No wonder everyone avoids you on Monday mornings. Must be that frown.",
+			[2] = "According to a study by Marmite, they found that the best way for people to smile on Monday mornings is to watch TV. Also on the list? Having sex and buying chocolate. Duh, right?",
+			[3] = [[Got time to complain?
+			 		Apparently all of those 'don't you just hate Monday' people complain for 34 minutes on average every Monday. 
+			 		That's compared to 22 minutes on any other day.
+			 		You count toward that average, too. Are you one of those annoying complainers?]],
+			[4] = [[In general, the stock market tends to rise a bit every day. Except for Mondays. They're the only day of the week that is more likely to fall instead of rise. 
+					Did you lose money on the stock market? You have permission to blame it on Mondays.]],
+		},
+		color = 5218047,
+	},
+	["Tue"] = {
+		avatar = "http://whitelion.bar/wp-content/uploads/2015/11/Tuesday-designstyle-colors-m-300x184.png",
+		status = "Tuesday... getting closer to friday.",
+		facts = {
+			[1] = "The day of the week Tuesday is named after Tiw, the Norse god of single combat, victory and glory.",
+			[2] = "2. Tiw is associated with Mars, the Roman god of war, which is why the day is Mardi in French, Martes in Spanish and Martedi in Italian.",
+			[3] = "Californians have barbecues less often on Tuesday than any other day of the week.",
+			[4] = "Under the rules of the Gregorian calendar, Christmas Eve falls less often on a Tuesday than any other day of the week...",
+			[5] = "According to a survey in 2002, Tuesday is the most productive day of the week in the workplace.",
+		},
+		color = 13979647,
+	},
+	["Wed"] = {
+		avatar = "https://yt3.ggpht.com/-NLkFN_OWDCg/AAAAAAAAAAI/AAAAAAAAAAA/2Godw1Km5Qw/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
+		status = "Wednesday!!!",
+		facts = {
+			[1] = "“Wednesday” is named after Woden, the most important God in the German pantheon, often identified with the Norse God Odin.",
+			[2] = "According to research by a tanning company, women look oldest at 3.30pm on Wednesdays. Yes, this research was done by a fairness cream company.",
+			[3] = "A recent survey in the US reported that bosses are most receptive to requests on a Wednesday.",
+			[4] = "The daughter in the Addams Family is called Wednesday Addams.",
+			[5] = "Wednesday has the best Hoboken wine specials.",
+			[6] = "There are movies on Pier A every Wednesday night during the summer.",
+			[7] = "The Wednesday before Easter is known as ‘Spy Wednesday’ referring to Judas’s betrayal of Jesus.", 
+			[8] = "Wednesday is also referred as ‘hump day’ because this day comes in middle of the week and after that its downhill towards weekend.",
+		},
+		color = 13979647,
+	},
+	["Thu"] = {
+		avatar = "https://2.bp.blogspot.com/-3SRKrxnavTU/V4JOjvziZHI/AAAAAAACQ7s/AVXLMSGE_xwKyyFzqkvWhDYSjjmdMeK2QCLcB/s1600/Happy%2BThursday%2B46.png",
+		status = "Almost Friday :)",
+		facts = {
+			[1] = "In countries that use the Sunday-first convention, Thursday is defined as the fifth day of the week.",
+			[2] = "The name is derived from Old English Þūnresdæg and Middle English Thuresday meaning “Thor’s Day.”",
+			[3] = "The astrological and astronomical sign of the planet Jupiter  is sometimes used to represent Thursday.",
+			[4] = "In the Christian tradition, Maundy Thursday or Holy Thursday is the Thursday before Easter — the day on which the Last Supper occurred. Also known as Sheer Thursday in the United Kingdom, it is traditionally a day of cleaning and giving out Maundy money there.",
+			[5] = "In the Eastern Orthodox Church, Thursdays are dedicated to the Apostles and Saint Nicholas.",
+			[6] = "In the Thai solar calendar, the colour associated with Thursday is orange.",
+			[7] = "In the USSR of the 1970s and 1980s Thursday was the “Fish Day”, when the nation’s foodservice establishments were supposed to serve fish (rather than meat) dishes.", 
+			[8] = "In some high schools in the United States during the 1950s and the 1960s, it was believed that if someone wore green on Thursdays, it meant that he or she was gay.",
+		},
+		color = 9043791,
+	},
+	["Fri"] = {
+		avatar = "http://az616578.vo.msecnd.net/files/2016/01/30/635897159225680692-1306698594_Wknd.png",
+		status = "Oh god it's friday!!!!!!!!",
+		facts = {
+			[1] = "In the U.S. they say about Friday «TGIF» – of «Thank God It’s Friday» («Thank you, Lord, today is Friday!”) Or another version of «Take Good In Fridays» («Happy holidays”)",
+			[2] = "On ancient legends Wednesday and Friday – two angels, who will make a person into heaven.",
+			[3] = "20 years ago, the collective unconscious fear of Friday the precipitated on 13th number, the American psychologist Donald Dossey coined a special term, “Paraskavedekatriafobiya.” According to the results of his research, in this day America loses $ 900 million due to decline of productivity.",
+			[4] = "“Friday” – the proper name of the character of the novel by Daniel Defoe’s “Robinson Crusoe”, the native and the novel’s heroine, “Friday, which kills” the science fiction of Heinlein – spy and trained killer.",
+			[5] = "In Islam, Friday – a day off. It is believed that this day would come to an end. In Christianity, Friday – the day that was executed by Jesus Christ.",
+			[6] = "In most places, where the adopted five-day week, Friday – the last working day before the weekend and is therefore seen as an occasion for celebration and relaxation – “Friday’s syndrome.”",
+			[7] = "In Catholic countries Friday – the sixth, not fifth day of the week.", 
+			[8] = "In old Germanic tribes, the day of the week was dedicated to the goddess Freya, which is why in English Friday called Friday.",
+		},
+		color = 16019010,
+	},
+}
