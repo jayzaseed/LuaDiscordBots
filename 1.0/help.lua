51d2c30cc19b029f00f1f11fc9ba72da0a2ec04f
John Sandler <priciel@protonmail.com>
2016-11-27 14:01:40 +0100

    Update help.lua

--- 1.0/help.lua
+++ 1.0/help.lua
@@ -1,8 +1,13 @@
+func = require("funciones")
+
 WhichDay = os.date("%a")
 date = require("WeekBotModules/Week")
 
+local Promote = "[Add me to your server!]('https://discordapp.com/oauth2/authorize?client_id=250995355653505024&scope=bot&permissions=0')"
+
 return {
 	["!fact"] = {
+		["description"] = Promote, 
 		["color"] = date[WhichDay].color,
 		["fields"] = {
 			{
@@ -13,6 +18,7 @@ return {
 		},
 	},
 	["!help"] = {
+		["description"] = Promote, 
 		["color"] = date[WhichDay].color,
 		["fields"] = {
 			{
@@ -23,11 +29,12 @@ return {
 		},
 	},
 	["empty"] = {
+		["description"] = Promote, 
 		["color"] = date[WhichDay].color,
 		["fields"] = {
 			{
 				name = "Available help commands", 
-				value =  "!help !fact\n!help !help.",
+				value =  "!help !fact\n!help !help",
 				inline = false
 			},
 		},
